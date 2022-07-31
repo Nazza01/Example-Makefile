f0d696cc4b623cb922c928dcc58fdd6722c21bf7
Nathanael <nate_thomas_95@hotmail.com>
2022-07-31 11:52:00 +0930

    Update Valgrind.mk
    Updating for new 42valgrind container now supporting logging and copying over logs from container to host with full leak checking, also updating 42valgrind as well

--- Valgrind.mk
+++ Valgrind.mk
@@ -1,27 +1,30 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Valgrind.mk                                        :+:      :+:    :+:    #
+#    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/01 20:23:35 by Nathanael         #+#    #+#              #
-#    Updated: 2022/07/10 16:18:42 by Nathanael        ###   ########.fr        #
+#    Updated: 2022/07/31 11:50:48 by Nathanael        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# **************************************************************************** #
+################################################################################
 #							Edit this section								   #
-NAME		=	SetFire
+NAME		=	Bureaucrat
 COMPILER	=	c++
 SOURCE_EXT	=	cpp
 COMP_STD	=	-std=c++98
-COMP_FLAGS	=	$(COMP_WALL) -g
-VALGRND_NAME=	v_ex01
+COMP_FLAGS	=	-Wall -Wextra -Werror -g
+VALGRND_NAME=	ex00
 # 																			   #
-# **************************************************************************** #
+################################################################################
 
-#				Progress Bar
+
+################################################################################
+#								Progress Bar								   #
+################################################################################
 ifndef ECHO
 T := $(shell $(MAKE) $(MAKECMDGOALS) --no-print-directory \
 	-nrRf $(firstword $(MAKEFILE_LIST)) \
@@ -32,58 +35,114 @@ C = $(words $N)$(eval N := x $N)
 ECHO = echo "`expr " [\`expr $C '*' 100 / $T\`" : '.*\(....\)$$'`%]"
 endif
 
-#				Directories, Files, Flags and Utilities
+################################################################################
+#								Colours										   #
+################################################################################
+CB_BLK		=	$(shell tput setab 0)
+CB_RED		=	$(shell tput setab 1)
+CB_GRN		=	$(shell tput setab 2)
+CB_YEL		=	$(shell tput setab 3)
+CB_BLU		=	$(shell tput setab 4)
+CB_MAG		=	$(shell tput setab 5)
+CB_CYN		=	$(shell tput setab 6)
+CB_WHT		=	$(shell tput setab 7)
+CT_BLK		=	$(shell tput setaf 0)
+CT_RED		=	$(shell tput setaf 1)
+CT_GRN		=	$(shell tput setaf 2)
+CT_YEL		=	$(shell tput setaf 3)
+CT_BLU		=	$(shell tput setaf 4)
+CT_MAG		=	$(shell tput setaf 5)
+CT_CYN		=	$(shell tput setaf 6)
+CT_WHT		=	$(shell tput setaf 7)
+CAL_RST		=	$(shell tput sgr0)
+
+C_CLN_MS	=	$(CB_WHT)$(CT_BLK)
+C_CLN_VA	=	$(CT_YEL)
+C_LNK_MS	=	$(CB_YEL)$(CT_BLK)
+C_LNK_V1	=	$(CT_RED)
+C_LNK_V2	=	$(CT_BLU)
+C_BLD_MS	=	$(CB_BLU)$(CT_BLK)
+C_BLD_VA	=	$(CT_MAG)
+C_FIN_MS	=	$(CB_GRN)$(CT_BLK)
+C_FIN_VA	=	$(CT_BLU)
+
+################################################################################
+#								Directories/Files							   #
+################################################################################
 SRC_DIR		=	./sources
-HDR_DIR		=	./headers
+HDR_DIR		=	./includes
 BUILD_DIR	=	./build
 
 SOURCES		=	$(shell find $(SRC_DIR) -name '*.$(SOURCE_EXT)')
 OBJECTS		=	$(SOURCES:$(SRC_DIR)/%.$(SOURCE_EXT)=$(BUILD_DIR)/%.o)
 DEPS		=	$(OBJECTS:.o=.d)
 
-CFLAGS		+=	$(COMP_STD) $(COMP_FLAGS)
+LOG			=	$(shell find . -name '*.log')
+################################################################################
+#								Flags/Utilities								   #
+################################################################################
+CFLAGS		+=	-$(COMP_STD) $(COMP_FLAGS)
 LDFLAGS		+=	
 INCLUDES	=	-I $(HDR_DIR)
 
-CLEAN_LST	=	$(NAME) $(BUILD_DIR)
+CLEAN_LST	=	$(NAME) $(LOG)
+FCLN_LST	=	$(BUILD_DIR) $(LOG)
 
 RM			=	rm -rf
 MKDIR		=	mkdir -p
 
-# 				 Start Rules
+################################################################################
+#								Make Rules									   #
+################################################################################
+.DELETE_ON_ERROR:
+.SILENT:
 
-# Default rule to make the program
 all: $(NAME)
+	$(ECHO) "${C_FIN_MS}  Ready to Run  ${CAL_RST}	${C_FIN_VA}$(NAME)${CAL_RST}"
 
-# Makes the build directory, links the object files to the program
 $(NAME): $(OBJECTS)
-	@$(ECHO) $@ ready to be run
-	@$(MKDIR) $(BUILD_DIR)
-	@$(COMPILER) $(OBJECTS) $(LDFLAGS) -o $@
+	$(ECHO) "${C_BLD_MS}    Building    ${CAL_RST}	${C_BLD_VA}$@${CAL_RST}"
+	$(MKDIR) $(BUILD_DIR)
+	$(COMPILER) $(OBJECTS) $(LDFLAGS) -o $@
 
-# Uses docker to spin up a valgrind container and makes the files there
-v valgrind: all
-	@$(ECHO) Cleaning any prior valgrind containers with the name $(VALGRND_NAME)
-	@docker stop $(VALGRND_NAME) || true && docker rm $(VALGRND_NAME) || true
-	@$(ECHO) Creating valgrind docker named: $(VALGRND_NAME)
-	@docker run --name $(VALGRND_NAME) -dit karek/valgrind:latest
-	@$(ECHO) Copying $(HDR_DIR) $(SRC_DIR) to $(VALGRND_NAME)
-	@docker cp $(SRC_DIR) $(VALGRND_NAME):/valgrind/sources
-	@docker cp $(HDR_DIR) $(VALGRND_NAME):/valgrind/headers
-	@docker cp Makefile $(VALGRND_NAME):/valgrind/Makefile
-	@$(ECHO) Using makefile to create $(NAME)
-	@docker exec $(VALGRND_NAME) make
-	@$(ECHO) Using valgrind to output results for $(NAME)
-	@docker exec $(VALGRND_NAME) valgrind ./$(NAME)
+c clean:
+	$(ECHO) "${C_CLN_MS}    Cleaning    ${CAL_RST}	${C_CLN_VA}$(CLEAN_LST)${CAL_RST}"
+	$(ECHO) "${C_CLN_MS}    Cleaning    ${CAL_RST}	${C_CLN_VA}$(FCLN_LST)${CAL_RST}"
+	$(RM) $(CLEAN_LST) $(FCLN_LST)
 
-# Include the dependancies into the compiler so all files can be built
+cc:
+	clear
+	$(ECHO) "${C_CLN_MS} Screen Cleared ${CAL_RST}"
+	$(MAKE) c
+
+r: c all
+
+m: all
+	leaks -atExit -- ./$(NAME) > leak_out.txt || true
+	grep -E '(Process) [0-9]{1,}: [0-9]{1,} (leak)' leak_out.txt
+	grep -E '(ROOT LEAK): <' leak_out.txt || true
+	rm leak_out.txt
+
+v:
+	$(ECHO) Cleaning any prior valgrind containers with the name $(VALGRND_NAME)
+	docker stop $(VALGRND_NAME) || true && docker rm $(VALGRND_NAME) || true
+	$(ECHO) Creating valgrind docker named: $(VALGRND_NAME)
+	docker run --name $(VALGRND_NAME) -dit livingsavage/42valgrind:v4
+	$(ECHO) Copying $(HDR_DIR) $(SRC_DIR) to $(VALGRND_NAME)
+	docker cp $(SRC_DIR) $(VALGRND_NAME):/code/sources
+	docker cp $(HDR_DIR) $(VALGRND_NAME):/code/includes
+	docker cp Makefile $(VALGRND_NAME):/code/Makefile
+	$(ECHO) Using makefile to create $(NAME)
+	docker exec $(VALGRND_NAME) make
+	$(ECHO) Using valgrind to log results for $(NAME)
+	docker exec $(VALGRND_NAME) valgrind --log-file=$(VALGRND_NAME).log -- ./$(NAME) > $(NAME).log 2>&1
+	docker cp $(VALGRND_NAME):/code/$(VALGRND_NAME).log .
+	echo "All done, Valgrind leak check logs are inside $(VALGRND_NAME).log"
+	echo "Program output is inside of $(NAME).log"
+
 -include $(DEPS)
 
-# Builds the object files from the source directory using the header files and
-# 	dependacies, ie: source/main.cpp is linked to object file build/main.o
-# From that, the $(NAME) rule can determine all the function definitions inside
-# 	each object file
 $(BUILD_DIR)/%.o : $(SRC_DIR)/%.$(SOURCE_EXT)
-	$(ECHO) Link $< to $@
+	$(ECHO) "${C_LNK_MS}    Linking     ${CAL_RST}	${C_LNK_V1}$< ${CAL_RST}${CT_WHT}to ${CAL_RST}${C_LNK_V2}$@ ${CAL_RST}"
 	$(MKDIR) $(@D)
-	$(COMPILER) $(CFLAGS) $(INCLUDES) -MP -MMD -c $< -o $@
+	$(COMPILER) $(CFLAGS) $(INCLUDES) -MMD -MP -c $< -o $@

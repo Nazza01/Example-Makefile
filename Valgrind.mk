5f39dec9c05475c7e7cedc8552387a52df43cfff
Nathanael <nate_thomas_95@hotmail.com>
2022-07-31 15:00:50 +0930

    Update Valgrind.mk
    Adding makefile from 42valgrind - see repo for changes

--- Valgrind.mk
+++ Valgrind.mk
@@ -6,7 +6,7 @@
 #    By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/01 20:23:35 by Nathanael         #+#    #+#              #
-#    Updated: 2022/07/31 11:50:48 by Nathanael        ###   ########.fr        #
+#    Updated: 2022/07/31 14:51:19 by Nathanael        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -123,22 +123,39 @@ m: all
 	grep -E '(ROOT LEAK): <' leak_out.txt || true
 	rm leak_out.txt
 
+
 v:
-	$(ECHO) Cleaning any prior valgrind containers with the name $(VALGRND_NAME)
-	docker stop $(VALGRND_NAME) || true && docker rm $(VALGRND_NAME) || true
+	$(ECHO) Removing old $(VALGRND_NAME) docker containers
+	docker stop $(VALGRND_NAME) > $(VALGRND_NAME)_docker.log 2>&1 || true && docker rm $(VALGRND_NAME) >> $(VALGRND_NAME)_docker.log 2>&1 || true
 	$(ECHO) Creating valgrind docker named: $(VALGRND_NAME)
-	docker run --name $(VALGRND_NAME) -dit livingsavage/42valgrind:v4
-	$(ECHO) Copying $(HDR_DIR) $(SRC_DIR) to $(VALGRND_NAME)
-	docker cp $(SRC_DIR) $(VALGRND_NAME):/code/sources
+	docker run --name $(VALGRND_NAME) -dit livingsavage/42valgrind:v4 >> $(VALGRND_NAME)_docker.log
+	$(ECHO) Copying Makefile $(HDR_DIR) $(SRC_DIR) to $(VALGRND_NAME)
 	docker cp $(HDR_DIR) $(VALGRND_NAME):/code/includes
+	docker cp $(SRC_DIR) $(VALGRND_NAME):/code/sources
 	docker cp Makefile $(VALGRND_NAME):/code/Makefile
 	$(ECHO) Using makefile to create $(NAME)
-	docker exec $(VALGRND_NAME) make
+	docker exec $(VALGRND_NAME) make >> $(VALGRND_NAME)_docker.log
 	$(ECHO) Using valgrind to log results for $(NAME)
-	docker exec $(VALGRND_NAME) valgrind --log-file=$(VALGRND_NAME).log -- ./$(NAME) > $(NAME).log 2>&1
-	docker cp $(VALGRND_NAME):/code/$(VALGRND_NAME).log .
-	echo "All done, Valgrind leak check logs are inside $(VALGRND_NAME).log"
-	echo "Program output is inside of $(NAME).log"
+	docker exec $(VALGRND_NAME) valgrind --log-file=$(VALGRND_NAME)_leaks.log -- ./$(NAME) > $(NAME).log
+	$(ECHO) Copying over $(VALGRND_NAME)_leaks.log file
+	docker cp $(VALGRND_NAME):/code/$(VALGRND_NAME)_leaks.log .
+	$(ECHO) Done!
+	echo "The container $(VALGRND_NAME) is still running, if you want to go into the container type: docker attach $(VALGRND_NAME)"
+	echo "Typing [make vl] to view valgrind logs, or [make pl] to view program logs, will view the respective logs using less"
+
+h:
+	echo "The following logs contain the following: "
+	echo "Valgrind leak output - $(VALGRND_NAME)_leaks.log"
+	echo "Normal program output - $(NAME).log"
+	echo "Docker command output - $(VALGRND_NAME)_docker.log"
+	echo "Makefile output - $(VALGRND_NAME)_make.log"
+	echo "Typing [make vl] to view valgrind logs, or [make pl] to view program logs, will view the respective logs using less"
+
+vl:
+	less $(VALGRND_NAME)_leaks.log
+
+pl:
+	less $(NAME).log
 
 -include $(DEPS)
 

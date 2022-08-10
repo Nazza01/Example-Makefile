1f22ded8be137bb2646b5c72a6295f69527446a3
Nathanael <nate_thomas_95@hotmail.com>
2022-08-10 10:19:59 +0930

    Update CPPModules.mk
    Updated file to most up to date version of Makefile for CPP08

--- CPPModules.mk
+++ CPPModules.mk
@@ -6,17 +6,18 @@
 #    By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/01 20:23:35 by Nathanael         #+#    #+#              #
-#    Updated: 2022/07/31 01:04:54 by Nathanael        ###   ########.fr        #
+#    Updated: 2022/08/07 22:04:59 by Nathanael        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 ################################################################################
 #							Edit this section								   #
-NAME		=	ex00
+NAME		=	changeMe.out
 COMPILER	=	c++
 SOURCE_EXT	=	cpp
 COMP_STD	=	-std=c++98
 COMP_FLAGS	=	-Wall -Wextra -Werror -g
+VALGRND_NAME=	changeMe
 # 																			   #
 ################################################################################
 
@@ -77,6 +78,8 @@ SOURCES		=	$(shell find $(SRC_DIR) -name '*.$(SOURCE_EXT)')
 OBJECTS		=	$(SOURCES:$(SRC_DIR)/%.$(SOURCE_EXT)=$(BUILD_DIR)/%.o)
 DEPS		=	$(OBJECTS:.o=.d)
 
+SHRUB_FL	=	$(shell find . -name '**_shrubbery')
+LOG			=	$(shell find . -name '*.log')
 ################################################################################
 #								Flags/Utilities								   #
 ################################################################################
@@ -84,8 +87,8 @@ CFLAGS		+=	-$(COMP_STD) $(COMP_FLAGS)
 LDFLAGS		+=	
 INCLUDES	=	-I $(HDR_DIR)
 
-CLEAN_LST	=	$(NAME)
-FCLN_LST	=	$(BUILD_DIR)
+CLEAN_LST	=	$(NAME) $(LOG)
+FCLN_LST	=	$(BUILD_DIR) $(SHRUB_FL) $(LOG)
 
 RM			=	rm -rf
 MKDIR		=	mkdir -p
@@ -121,6 +124,47 @@ m: all
 	grep -E '(Process) [0-9]{1,}: [0-9]{1,} (leak)' leak_out.txt
 	grep -E '(ROOT LEAK): <' leak_out.txt || true
 	rm leak_out.txt
+
+cv:
+	$(ECHO) Removing old $(VALGRND_NAME) docker containers
+	docker stop $(VALGRND_NAME) 2>&1 || true && docker rm $(VALGRND_NAME) 2>&1 || true
+
+v val:
+	$(ECHO) Removing old $(VALGRND_NAME) docker containers
+	docker stop $(VALGRND_NAME) > $(VALGRND_NAME)_docker.log 2>&1 || true && docker rm $(VALGRND_NAME) >> $(VALGRND_NAME)_docker.log 2>&1 || true
+	$(ECHO) Creating valgrind docker named: $(VALGRND_NAME)
+	docker run --name $(VALGRND_NAME) -dit livingsavage/42valgrind:v4 >> $(VALGRND_NAME)_docker.log
+	$(ECHO) Copying Makefile $(HDR_DIR) $(SRC_DIR) to $(VALGRND_NAME)
+	docker cp $(HDR_DIR) $(VALGRND_NAME):/code/includes
+	docker cp $(SRC_DIR) $(VALGRND_NAME):/code/sources
+	docker cp Makefile $(VALGRND_NAME):/code/Makefile
+	$(ECHO) Using makefile to create $(NAME)
+	docker exec $(VALGRND_NAME) make >> $(VALGRND_NAME)_docker.log
+	$(ECHO) Using valgrind to log results for $(NAME)
+	docker exec $(VALGRND_NAME) valgrind --log-file=$(VALGRND_NAME)_leaks.log -- ./$(NAME) > $(NAME).log
+	$(ECHO) Copying over $(VALGRND_NAME)_leaks.log file
+	docker cp $(VALGRND_NAME):/code/$(VALGRND_NAME)_leaks.log .
+	$(ECHO) Done!
+	echo "The container $(VALGRND_NAME) is still running, if you want to go into the container type: docker attach $(VALGRND_NAME)"
+	echo "Typing [make vl] to view valgrind logs, or [make pl] to view program logs, will view the respective logs using less"
+
+vc valclean:
+	$(ECHO) Removing old $(VALGRND_NAME) docker containers
+	docker stop $(VALGRND_NAME) 2>&1 || true && docker rm $(VALGRND_NAME) 2>&1 || true
+
+vh valhelp:
+	echo "The following logs contain the following: "
+	echo "Valgrind leak output - $(VALGRND_NAME)_leaks.log"
+	echo "Normal program output - $(NAME).log"
+	echo "Docker command output - $(VALGRND_NAME)_docker.log"
+	echo "Makefile output - $(VALGRND_NAME)_make.log"
+	echo "Typing [make vl] to view valgrind logs, or [make pl] to view program logs, will view the respective logs using less"
+
+vl vallog:
+	less $(VALGRND_NAME)_leaks.log
+
+pl proglog:
+	less $(NAME).log
 
 -include $(DEPS)
 

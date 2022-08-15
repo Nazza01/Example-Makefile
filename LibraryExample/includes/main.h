83d5ae9de1e0fe07e363eb16b66c252b244a3f33
Nazza01 <nate_thomas_95@hotmail.com>
2022-08-15 22:46:38 +0930

    Adding progress, implemented silent feature for libraries

--- LibraryExample/includes/main.h
+++ LibraryExample/includes/main.h
@@ -6,14 +6,18 @@
 /*   By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 23:11:08 by Nathanael         #+#    #+#             */
-/*   Updated: 2022/08/15 21:58:34 by Nathanael        ###   ########.fr       */
+/*   Updated: 2022/08/15 22:41:49 by Nathanael        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
-# include "./imported/libft.h"
-# include "./imported/ft_printf.h"
+# include "imported/libft.h"
+# include "imported/ft_printf.h"
+# include "imported/get_next_line.h"
+# include "imported/personal.h"
+
+# include <stdio.h>
 
 #endif
--- LibraryExample/sources/main.c
+++ LibraryExample/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 18:02:22 by Nathanael         #+#    #+#             */
-/*   Updated: 2022/08/15 11:18:38 by Nathanael        ###   ########.fr       */
+/*   Updated: 2022/08/15 22:45:27 by Nathanael        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 int	main(void)
 {
-	ft_printf("Hello from ft_printf");
+	int		fd;
+	char	*line;
+
+	ft_putstr_fd("ft_putchar_fd\n", 1);
+	ft_putchar_fd('a', 1);
+	ft_putchar_fd('\n', 1);
+	fd = open("Makefile", O_RDONLY);
+	line = get_next_line(fd);
+	ft_printf("First Line: %s\n", line);
+	free(line);
+	
+	ft_printf("Hello from ft_printf\n");
+	
+	ft_printf("The absolute value of -100 is: %i\n", ft_abs(-100));
 	return (0);
 }

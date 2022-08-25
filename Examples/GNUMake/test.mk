03eaae725d3b43d0a4a683463d96c925c656ef25
Nazza01 <nate_thomas_95@hotmail.com>
2022-08-25 22:45:01 +0930

    Adding presentation subfolders with ideas, need to write slides up but most of structure is there

--- Examples/GNUMake/test.mk
+++
@@ -1,4 +0,0 @@
-SOURCES		:=	$(wildcard sources/*.c)
-INCLUDES	:=	$(wildcard includes/*.h)
-OBJECTS		:=	$(SOURCES:sources/%.c=build/objects/%.o)
-
---
+++ Presentation/7-NestedAllExtras/src/main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 20:37:41 by Nathanael         #+#    #+#             */
+/*   Updated: 2022/08/25 22:32:37 by Nathanael        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fullexample.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("The length of Hello is: %i\n", ft_strlen("Hello"));
+	return (0);
+}
